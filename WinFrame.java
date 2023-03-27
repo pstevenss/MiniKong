ec9b3fa70ebd683808a404a23d6f41b6dc00b096
justint <justintolbertpro@gmail.com>
2023-03-27 13:13:54 -0400

    New version of the game with background and winning and losing.

---
+++ WinFrame.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WinFrame here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WinFrame extends World
+{
+
+    /**
+     * Constructor for objects of class WinFrame.
+     * 
+     */
+    public WinFrame()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(960, 540, 1); 
+        addObject(new ContinueButton(),750,370);
+    }
+}
